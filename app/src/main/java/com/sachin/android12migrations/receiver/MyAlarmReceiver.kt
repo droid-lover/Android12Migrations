92b405d80b83c6d1c705b49e330f07a9ca0fe0b3
droid-lover <ersachin20@gmail.com>
2022-02-08 21:01:25 +0530

    Implementing basic Alarm broadcast receiver class

--- app/src/main/java/com/sachin/android12migrations/receiver/MyAlarmReceiver.kt
+++ app/src/main/java/com/sachin/android12migrations/receiver/MyAlarmReceiver.kt
@@ -3,12 +3,14 @@ package com.sachin.android12migrations.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import com.sachin.android12migrations.notifications.NotificationHelper
 
 class MyAlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         context?.also {
+            Toast.makeText(it,"Coming",Toast.LENGTH_SHORT).show()
             val notificationHelper = NotificationHelper(context)
             NotificationHelper(context).makeNotification(notificationHelper.createNotificationBuilder(),1001)
         }
